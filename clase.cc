ed1e6e72bd877eb5757f7a2c10aa8b6a50c9d911
ivanrejonm <31378786+ivanrejonm@users.noreply.github.com>
2017-10-30 13:27:33 -0600

    Add files via upload

--- clase.cc
+++ clase.cc
@@ -1,11 +1,11 @@
-//clase
+//clase1
 //Ivàn Rejòn
 //30/oct/17
 #include <iostream>
 using namespace std;
 int main() {
   size_t talla=0;
-  int *edad[talla];
+  int *edad=new int[talla];
   int suma=0;
   for(int i=0;i<talla;i++)
     {
@@ -16,7 +16,7 @@ int main() {
   suma+=edad[i];
     }
  for (int j=0;j<talla;j++)
-   cout<<edad[j]<<","
+   cout<<edad[j]<<",";
  cout<<"la edad promedio es: "<<(suma)/talla<<endl;
 
  delete []edad;
